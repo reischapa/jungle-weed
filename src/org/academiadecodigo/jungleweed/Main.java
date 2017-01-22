19a4c9f0f814317aae742ef4ce514fbe762ce3f4
João Chapa <reischapa@gmail.com>
2017-01-22 15:33:59 +0000

    Initial commit

---
+++ src/org/academiadecodigo/jungleweed/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by codecadet on 1/22/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("solanum tuberosum");
+    }
+
+}
---
+++ src/org/academiadecodigo/jungleweed/card/CardColor.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.jungleweed.card;
+
+/**
+ * Created by codecadet on 1/22/17.
+ */
+public class CardColor {
+}
