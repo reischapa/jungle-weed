9a7e99fdb86615a5818d12f26b147a951895085b
Luizord <luizord@email.com>
2017-01-22 18:33:45 +0000

    O luizord so faz merda...

--- src/org/academiadecodigo/jungleweed/Game.java
+++ src/org/academiadecodigo/jungleweed/Game.java
@@ -5,17 +5,14 @@ import org.academiadecodigo.jungleweed.card.CardFactory;
 import org.academiadecodigo.jungleweed.player.Player;
 import org.academiadecodigo.jungleweed.player.PlayerFactory;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 /**
  * Created by codecadet on 1/22/17.
  */
 public class Game {
 
     public static int NUMBER_PLAYERS = 4;
+    public static int NUMBER_CARDS_TOTAL = 80;
+    public static int NUMBER_HAND_CARDS;
 
     private Player[] players;
     private CompareType compareType;
@@ -26,19 +23,19 @@ public class Game {
         //TODO
         this.players = new Player[NUMBER_PLAYERS];
         this.compareType = CompareType.SHAPE;
+        NUMBER_HAND_CARDS = NUMBER_CARDS_TOTAL / NUMBER_PLAYERS;
     }
 
 
     public void init() {
         for(int i=0; i<NUMBER_PLAYERS;i++){
-            players[i]= PlayerFactory.getNewPlayer(20);
-            int maxPlayerCards = players[i].getMaxPossibleCards();
-            Card[] cards = new Card[maxPlayerCards];
-            for(int j=0; j<maxPlayerCards; j++){
+            players[i]= PlayerFactory.getNewPlayer(NUMBER_CARDS_TOTAL);
+            Card[] cards = new Card[NUMBER_HAND_CARDS];
+            for(int j=0; j < NUMBER_HAND_CARDS; j++){
                 cards[j]=CardFactory.getRandomCard();
             }
             players[i].receiveCards(cards);
-            System.out.println(players[i]);
+            //System.out.println(players[i]);
         }
 
     }
@@ -94,7 +91,7 @@ public class Game {
             case SHAPE:
                 return this.compareShape(c1, c2);
             default:
-                System.out.println("Piço");
+                System.out.println("Deu Merda wtf is Happening ?!");
                 return this.compareShape(c1, c2);
         }
 
