571ebaab0984394722f8c10ac1eb900b822fa05f
João Chapa <reischapa@gmail.com>
2017-01-27 17:34:01 +0000

    Variable players in the controller

--- src/org/academiadecodigo/jungleweed/SFXController.java
+++ src/org/academiadecodigo/jungleweed/SFXController.java
@@ -5,13 +5,20 @@ import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
+import java.util.IllegalFormatCodePointException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by codecadet on 1/27/17.
  */
 public class SFXController implements KeyboardHandler {
 
-    private final int[] revealKeys = {KeyboardEvent.KEY_1, KeyboardEvent.KEY_4, KeyboardEvent.KEY_7, KeyboardEvent.KEY_0};
-    private final int[] grabKeys = {KeyboardEvent.KEY_Q, KeyboardEvent.KEY_R, KeyboardEvent.KEY_Y, KeyboardEvent.KEY_O} ;
+    private final Integer[] allRevealKeys = {KeyboardEvent.KEY_1, KeyboardEvent.KEY_4, KeyboardEvent.KEY_7, KeyboardEvent.KEY_0};
+    private final Integer[] allGrabKeys = {KeyboardEvent.KEY_Q, KeyboardEvent.KEY_R, KeyboardEvent.KEY_Y, KeyboardEvent.KEY_O};
+
+    private List<Integer> revealKeys;
+    private List<Integer> grabKeys;
 
     private int numPlayers;
 
@@ -25,13 +32,52 @@ public class SFXController implements KeyboardHandler {
         SFXController c = new SFXController(4);
 
 
+
     }
 
     public SFXController(int numPlayers) {
 
+        if (numPlayers > this.allRevealKeys.length || numPlayers < 2) {
+            throw new IllegalArgumentException();
+        }
+
+        this.revealKeys = new LinkedList<>();
+        this.grabKeys = new LinkedList<>();
+
+        switch (numPlayers) {
+            case 2:
+                this.revealKeys.add(this.allRevealKeys[0]);
+                this.revealKeys.add(this.allRevealKeys[3]);
+                this.grabKeys.add(this.allGrabKeys[0]);
+                this.grabKeys.add(this.allGrabKeys[3]);
+                break;
+            case 3:
+                this.revealKeys.add(this.allRevealKeys[0]);
+                this.revealKeys.add(this.allRevealKeys[2]);
+                this.revealKeys.add(this.allRevealKeys[3]);
+                this.grabKeys.add(this.allGrabKeys[0]);
+                this.grabKeys.add(this.allGrabKeys[2]);
+                this.grabKeys.add(this.allGrabKeys[0]);
+                break;
+            case 4:
+                this.revealKeys.add(this.allRevealKeys[0]);
+                this.revealKeys.add(this.allRevealKeys[1]);
+                this.revealKeys.add(this.allRevealKeys[2]);
+                this.revealKeys.add(this.allRevealKeys[3]);
+                this.grabKeys.add(this.allGrabKeys[0]);
+                this.grabKeys.add(this.allGrabKeys[1]);
+                this.grabKeys.add(this.allGrabKeys[2]);
+                this.grabKeys.add(this.allGrabKeys[3]);
+                break;
+        }
+
+
         this.numPlayers = numPlayers;
 
         this.game = new Game();
+
+        this.game.init();
+        this.game.start();
 
         this.keyboard = new Keyboard(this);
 
@@ -43,13 +89,14 @@ public class SFXController implements KeyboardHandler {
 
     private void constructEventListeners() {
 
-        for (int i = 0; i < this.revealKeys.length; i++) {
+
+        for (int i = 0; i < this.revealKeys.size(); i++) {
 
             KeyboardEvent revealEvent = new KeyboardEvent();
             KeyboardEvent grabEvent = new KeyboardEvent();
 
-            revealEvent.setKey(this.revealKeys[i]);
-            grabEvent.setKey(this.grabKeys[i]);
+            revealEvent.setKey(this.revealKeys.get(i));
+            grabEvent.setKey(this.grabKeys.get(i));
 
             revealEvent.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
             grabEvent.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
@@ -62,22 +109,21 @@ public class SFXController implements KeyboardHandler {
     }
 
 
-
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
 
-        for (int i = 0; i < this.revealKeys.length; i++) {
+        for (int i = 0; i < this.allRevealKeys.length; i++) {
 
             if (keyboardEvent.getKey() == KeyboardEvent.KEY_RIGHT) {
                 System.exit(0);
             }
 
-            if (keyboardEvent.getKey() == this.revealKeys[i]) {
-                System.out.println(i);
+            if (keyboardEvent.getKey() == this.revealKeys.get(i)) {
+                this.game.getPlayerFaceUpCard(i);
             }
 
-            if (keyboardEvent.getKey() == this.grabKeys[i]) {
-                System.out.println("grabbing " + i);
+            if (keyboardEvent.getKey() == this.allGrabKeys[i]) {
+                this.game.grabTotem(i);
             }
 
         }
